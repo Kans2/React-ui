798277e4d46cc9fbaf4f22b4befa63fe55532f28
Kans2 <142505200+Kans2@users.noreply.github.com>
2024-05-16 03:40:12 +0000

    updated

--- my-ui/src/App.js
+++ my-ui/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-
+import { BiArchive } from "react-icons/bi";
 function App() {
   return (
-    <div className="App">
-       <h1>hello world</h1>
-      
+    <div className="container">
+      <header>
+        <div>
+          <h1 className='select'><BiArchive/></h1>
+        </div>
+      <h1>Your Appoinments</h1>
+      </header>
     </div>
   );
 }
